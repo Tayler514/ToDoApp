0e9535487bd3c5e461ae9969383b5cc331ced96e
Tayler Anderson <tayleranderson@Taylers-MacBook-Air.local>
2021-03-05 19:11:24 -0600

    deploy my app to github

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./app.css";
+import TodoList from "./components/TodoList";
+
+function App() {
+  return (
+    <div className="todo-app">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
